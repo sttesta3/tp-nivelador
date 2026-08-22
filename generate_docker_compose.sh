843a078eaca0866e46dbdb297d34bb8f78cfaf76
sttesta3 <sttesta@fi.uba.ar>
2026-08-22 15:56:26 -0300

    Script que genera los clientes. Todos los clientes connection refused

---
+++ generate_docker_compose.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+	echo "Usage: $0 <cantidad de clientes>"
+	exit 1
+elif [[ ! $1 =~ ^[+-]?[0-9]*\.?[0-9]+$ ]]; then 
+	echo "La cantidad de clientes debe ser numerica y positiva"
+	exit 1
+elif [[ $1 -lt 1 ]]; then
+	echo "La cantidad de clientes debe ser positiva"
+	exit 1
+fi
+
+echo 'services:
+  server:
+    build:
+      context: ./services/server
+      dockerfile: Dockerfile
+    container_name: server
+    environment:
+      - PYTHONUNBUFFERED=1
+      - SERVER_HOST=server
+      - SERVER_PORT=5678' > docker-compose2.yaml
+
+for ((i=0; i<$1; i++)); do echo "
+  client_$i:
+    build:
+      context: ./services/client
+      dockerfile: Dockerfile
+    container_name: client_$i
+    depends_on:
+      - server
+    environment:
+      - AGENCY_ID=$i
+      - SERVER_HOST=server
+      - SERVER_PORT=5678" >> docker-compose2.yaml 
+done
