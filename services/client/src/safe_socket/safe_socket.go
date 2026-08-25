8dd29f7a417896fbda53c00c58aceb78010ecf1c
sttesta3 <sttesta@fi.uba.ar>
2026-08-25 16:10:34 -0300

    protegido contra small write. No entiendo como proteger contra small read, ya que no puedo saber si el mensaje esta completo o no (solo puedo saber si hubo error o EOF, que significa cierre de conexion

--- services/client/src/safe_socket/safe_socket.go
+++ services/client/src/safe_socket/safe_socket.go
@@ -5,9 +5,13 @@ import "io"
 //TODO: Complete with a short-read/short-write tolerant implementation
 
 func SendAll(socket io.Writer, bytes []byte) error {
-	_, err := socket.Write(bytes)
-	if err != nil {
-		return err
+	bytes_written := 0
+	for bytes_written < len(bytes) {
+		bytes_written_on_this_attempt, err := socket.Write(bytes[bytes_written:])
+		if err != nil {
+			return err
+		}
+		bytes_written += bytes_written_on_this_attempt
 	}
 	return nil
 }
